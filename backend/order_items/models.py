28b0f54c6d702e0275c005df24d2405ce0447986
Daniil Silin <daniil_silin@mail.ru>
2022-02-26 15:11:40 +0000

    Updated relations for order items

--- backend/order_items/models.py
+++ backend/order_items/models.py
@@ -7,6 +7,7 @@ class OrderItems(models.Model):
     name = models.CharField('Название позиции', max_length=255)
     price = models.FloatField(validators=[MinValueValidator(1)])
     quantity = models.IntegerField(default=1, validators=[MinValueValidator(1)])
+    order = models.ForeignKey(to='orders.Orders', on_delete=models.CASCADE)
 
     class Meta:
         db_table = "order_items"
