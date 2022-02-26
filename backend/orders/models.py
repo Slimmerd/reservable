cd4c770f27e12aca3b383b32194a1a45c02806b3
Daniil Silin <daniil_silin@mail.ru>
2022-02-26 15:11:26 +0000

    Updated relations for orders, added extra rows

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,17 +1,25 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-
 # Create your models here.
+from django.utils import timezone
+
+
 class Orders(models.Model):
     is_reservation = models.BooleanField(default=False)
     reserved_date = models.DateTimeField()
     persons = models.IntegerField(default=1, validators=[MinValueValidator(1)])
-    reservation_cost = models.FloatField()
+
     percent = models.IntegerField(validators=[MaxValueValidator(100)])
+    reservation_cost = models.FloatField()
     total = models.FloatField()
     total_after_percent = models.FloatField()
-    order_items = models.ForeignKey(to='order_items.OrderItems', on_delete=models.CASCADE)
+
+    comment = models.CharField(blank=True, max_length=512)
+    created_at = models.DateTimeField(default=timezone.now())
+
+    merchant = models.ForeignKey(to='merchants.Merchants', on_delete=models.CASCADE)
+    client = models.ForeignKey(to='clients.Client', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'orders'
