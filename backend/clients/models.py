209d65aa00bab532808121f8c6d47cb1cb300ae3
Daniil Silin <daniil_silin@mail.ru>
2022-02-26 15:12:23 +0000

    Updated relations for client model

--- backend/clients/models.py
+++ backend/clients/models.py
@@ -6,7 +6,6 @@ class Clients(models.Model):
     name = models.CharField('Имя', max_length=255)
     email = models.EmailField()
     phone = models.CharField('Телефон', max_length=11)
-    orders = models.ForeignKey(to='orders.Orders', on_delete=models.CASCADE)
 
     class Meta:
         db_table = "clients"
